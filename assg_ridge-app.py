4866bc4c96ab975b71857f730261cebeda06ca15
ras-km <110680434+ras-km@users.noreply.github.com>
2024-01-02 22:04:46 +0800

    Update assg_ridge-app.py

--- assg_ridge-app.py
+++ assg_ridge-app.py
@@ -7,7 +7,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import Ridge
 import pickle
 import joblib
 
