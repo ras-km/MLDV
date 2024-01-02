6ac5d88ba90c031922ea83d25603e7a8b90f33ee
ras-km <110680434+ras-km@users.noreply.github.com>
2024-01-02 20:27:53 +0800

    Update premium-app.py

--- premium-app.py
+++ premium-app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
