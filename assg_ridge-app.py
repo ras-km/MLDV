5a1751e51722ed6706800310a26cc520b03c17ec
ras-km <110680434+ras-km@users.noreply.github.com>
2024-01-03 23:42:04 +0800

    Update assg_ridge-app.py

--- assg_ridge-app.py
+++ assg_ridge-app.py
@@ -5,11 +5,11 @@ from sklearn.linear_model import Ridge
 import pickle
 import joblib
 
-st.title("""
-# Insurance Premium Prediction App
+st.title(Insurance Premium Prediction App)
 
-This app predicts the insurance premium based on user input.
-""")
+
+st.header(This app predicts the insurance premium based on user input)
+
 
 st.image('medical_insurance4.jpeg')
 
