4d8eff664b7e1ce9cbf645721f26f09d1623cafc
ras-km <110680434+ras-km@users.noreply.github.com>
2024-02-01 02:26:45 +0800

    Update assignment-app.py

--- assignment-app.py
+++ assignment-app.py
@@ -16,7 +16,7 @@ st.set_page_config(
 background_image = """
 <style>
 [data-testid="stAppViewContainer"] > .main {
-    background-image: url("https://images.pexels.com/photos/169789/pexels-photo-169789.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+    background-image: url("https://images.pexels.com/photos/2096622/pexels-photo-2096622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
